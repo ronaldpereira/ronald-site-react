be86036042526e98c861a3c0c35d9aa22cb252cf
Ronald Pereira <ronald.drp11@gmail.com>
2018-06-09 00:20:07 -0300

    added style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+import './assets/css/default.min.css';
 
-      </div>
-    );
-  }
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                
+            </div>
+        );
+    }
 }
 
 export default App;
